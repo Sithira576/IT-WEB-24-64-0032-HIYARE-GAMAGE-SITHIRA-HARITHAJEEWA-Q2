f0355f99101b9ea1442d584f2ea5a3fe88a042d3
Sithira576 <fedriknorth40@gmail.com>
2024-10-26 20:18:37 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+INSERT INTO Courses (CourseId, Name, LecturerName)
+VALUES
+(1, 'Web Development', 'M.M. Herath'),
+(2, 'Graphic Design', 'J.S.V. Piyasena'),
+(3, 'Mobile App Development', 'K.K.S. Dias'),
+(4, 'Java', 'U.H.S. Perera');
---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+INSERT INTO Students (StudentId, Name, City, CourseId)
+VALUES
+(1, 'Kasun Gamage', 'Kandy', 2),
+(2, 'Daniel Sam', 'Jaffna', 3),
+(3, 'Hansi Silva', 'Colombo', 1),
+(4, 'Ranidu Heath', 'Matara', 3),
+(5, 'Praneeth Wijesinghe', 'Kandy', 4),
+(6, 'Nuwani Herath', 'Rathnapura', 1);
